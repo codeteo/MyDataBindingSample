869ec8a799cd7dc030f9ee038f57d6fcacb2d492
teo <ramones.teo@gmail.com>
2017-07-14 15:32:42 +0300

    Displays weather icon

--- app/src/main/java/com/data/binding/main/ViewModel.java
+++ app/src/main/java/com/data/binding/main/ViewModel.java
@@ -1,12 +1,15 @@
 package com.data.binding.main;
 
+import android.databinding.BindingAdapter;
 import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.data.binding.domain.entities.CityWeather;
 import com.data.binding.domain.entities.User;
 import com.data.binding.domain.model.WeatherInteractor;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
 
@@ -18,10 +21,12 @@ import rx.functions.Action1;
 
 public class ViewModel implements GetUserCallback {
 
-    public static final String TAG = "VIEW-MODEL";
+    private static final String TAG = "VIEW-MODEL";
+    private static final String BASE_ICON_URL = "http://openweathermap.org/img/w/";
 
     public final ObservableField<String> name = new ObservableField<>();
     public final ObservableField<String> description = new ObservableField<>();
+    public final ObservableField<String> icon = new ObservableField<>();
 
     @NonNull
     private WeatherInteractor weatherInteractor;
@@ -37,9 +42,9 @@ public class ViewModel implements GetUserCallback {
                 .subscribe(new Action1<CityWeather>() {
                     @Override
                     public void call(CityWeather cityWeather) {
-                        Log.i(TAG, "call Do something here");
                         name.set(cityWeather.getName());
                         description.set(cityWeather.getWeather().get(0).getDescription());
+                        icon.set(createIconUrl(cityWeather.getWeather().get(0).getIcon()));
                     }
                 }, new Action1<Throwable>() {
                     @Override
@@ -58,5 +63,16 @@ public class ViewModel implements GetUserCallback {
     @Override
     public void onUserNotAvailable() {
         // notify user
+    }
+
+    @BindingAdapter({"bind:imageUrl"})
+    public static void loadImage(ImageView view, String imageUrl) {
+        Picasso.with(view.getContext())
+                .load(imageUrl)
+                .into(view);
+    }
+
+    private String createIconUrl(String icon) {
+        return BASE_ICON_URL + icon + ".png";
     }
 }
