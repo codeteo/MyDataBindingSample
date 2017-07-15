56576b32f5563f4897fba8ac1a542b2788d1e9bf
teo <ramones.teo@gmail.com>
2017-07-15 18:00:58 +0300

    Adds layout for SearchFragment

--- app/src/main/java/com/data/binding/main/fragments/search/SearchFragment.java
+++ app/src/main/java/com/data/binding/main/fragments/search/SearchFragment.java
@@ -1,8 +1,18 @@
 package com.data.binding.main.fragments.search;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.data.binding.R;
 import com.data.binding.main.fragments.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A screen where user can search for the weather in various places.
@@ -12,13 +22,25 @@ import com.data.binding.main.fragments.BaseFragment;
 
 public class SearchFragment extends BaseFragment {
 
+    @BindView(R.id.et_search) EditText etSearchView;
+    @BindView(R.id.rv_search) RecyclerView rvResults;
+
     public static SearchFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         SearchFragment fragment = new SearchFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
     }
 
 }
