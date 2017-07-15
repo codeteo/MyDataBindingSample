25b7f9949c2c305ecd6d95005cca24d01fc45b28
teo <ramones.teo@gmail.com>
2017-07-15 17:38:58 +0300

    Changes Fragment when user clicks on bottomBar tabs

--- app/src/main/java/com/data/binding/main/MainActivity.java
+++ app/src/main/java/com/data/binding/main/MainActivity.java
@@ -3,11 +3,15 @@ package com.data.binding.main;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.data.binding.R;
+import com.data.binding.main.fragments.favorites.FavoriteFragment;
 import com.data.binding.main.fragments.main.MainFragment;
+import com.data.binding.main.fragments.search.SearchFragment;
+import com.data.binding.main.fragments.settings.SettingsFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,6 +19,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.bb_bottom_bar_view) BottomNavigationView bottomBar;
+
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +38,25 @@ public class MainActivity extends AppCompatActivity {
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     switch (item.getItemId()) {
                         case R.id.action_home:
-
+                            fragment = MainFragment.newInstance();
                             break;
                         case R.id.action_search:
-
+                            fragment = SearchFragment.newInstance();
                             break;
                         case R.id.action_favorite:
-
+                            fragment = FavoriteFragment.newInstance();
                             break;
                         case R.id.action_settings:
-
+                            fragment = SettingsFragment.newInstance();
                             break;
 
                     }
-                    return false;
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fl_main_container, fragment)
+                            .commit();
+
+                    return true;
                 }
             });
 
