867492d3fc23d9de8aa5f1fad25eb5ffe59d5da8
teo <ramones.teo@gmail.com>
2017-07-15 17:15:56 +0300

    Adds listener to bottomBar

--- app/src/main/java/com/data/binding/main/MainActivity.java
+++ app/src/main/java/com/data/binding/main/MainActivity.java
@@ -1,14 +1,20 @@
 package com.data.binding.main;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.data.binding.R;
 import com.data.binding.main.fragments.main.MainFragment;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.bb_bottom_bar_view) BottomNavigationView bottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,29 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fl_main_container, MainFragment.newInstance())
                 .commit();
+
+        bottomBar.setOnNavigationItemSelectedListener(
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_home:
+
+                            break;
+                        case R.id.action_search:
+
+                            break;
+                        case R.id.action_favorite:
+
+                            break;
+                        case R.id.action_settings:
+
+                            break;
+
+                    }
+                    return false;
+                }
+            });
 
     }
 
